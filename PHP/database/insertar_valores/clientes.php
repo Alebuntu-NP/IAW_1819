a006c53fc3e4ed4e574067977eea4068cde14ba7
Alejandro314 <alejandro_roman_caballero@outlook.es>
2018-11-26 10:59:44 +0100

    Actualizacion_general

---
+++ PHP/database/insertar_valores/clientes.php
@@ -0,0 +1,74 @@
+<!--
+  Mostrar vehiculos
+-->
+<?php
+  include_once("./db_configuration.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB Example Using OpenShift DB Configuration</title>
+  </head>
+  <body>
+    <?php
+
+      //CREATING THE CONNECTION
+      $connection = new mysqli("localhost", "tf", "123456", "tf");
+      $connection->set_charset("utf8");
+
+      //TESTING IF THE CONNECTION WAS RIGHT
+      if ($connection->connect_errno) {
+          printf("Connection failed: %s\n", $connection->connect_error);
+          exit();
+      }
+
+      //MAKING A SELECT QUERY
+      /* Consultas de selección que devuelven un conjunto de resultados */
+      if ($result = $connection->query("select * from clientes;")) {
+
+          printf("<p>The select query returned %d rows.</p>", $result->num_rows);
+
+      ?>
+
+          <!-- PRINT THE TABLE AND THE HEADER -->
+          <table style="border:1px solid black">
+          <thead>
+            <tr>
+              <th>CodCliente</th>
+              <th>DNI</th>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>Direccion</th>
+              <th>Telefono</th>
+             </tr>
+          </thead>
+
+      <?php
+
+          //FETCHING OBJECTS FROM THE RESULT SET
+          //THE LOOP CONTINUES WHILE WE HAVE ANY OBJECT (Query Row) LEFT
+          while($obj = $result->fetch_object()) {
+              //PRINTING EACH ROW
+              echo "<tr>";
+              echo "<td>".$obj->CodCliente."</td>";
+              echo "<td>".$obj->DNI."</td>";
+              echo "<td>".$obj->Nombre."</td>";
+              echo "<td>".$obj->Apellidos."</td>";
+              echo "<td>".$obj->Direccion."</td>";
+              echo "<td>".$obj->Telefono."</td>";
+              echo "<td><a href='editar_cliente.php?codc=$obj->CodCliente'><img src='imagen.jpg' style='width:40px'></img></a></td>";
+              echo "</tr>";
+          }
+
+          //Free the result. Avoid High Memory Usages
+          $result->close();
+          unset($obj);
+          unset($connection);
+
+      } //END OF THE IF CHECKING IF THE QUERY WAS RIGHT
+
+    ?>
+  </body>
+</html>
---
+++ PHP/database/insertar_valores/editar_cliente.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+<?php if (!isset($_POST["codcl"])): ?>
+    <?php
+
+//CREATING THE CONNECTION
+$connection = new mysqli("localhost", "tf", "123456", "tf");
+$connection->set_charset("utf8");
+//TESTING IF THE CONNECTION WAS RIGHT
+if ($connection->connect_errno) {
+    printf("Connection failed: %s\n", $connection->connect_error);
+    exit();
+}
+//MAKING A SELECT QUERY
+/* Consultas de selección que devuelven un conjunto de resultados */
+$query = "select * from clientes where codcliente ='".$_GET['codc']."'";
+
+if ($result = $connection->query($query)) {
+    while($obj = $result->fetch_object()) {
+
+
+echo "<form method='post'>";
+echo "<fieldset>";
+echo "<span>CodCliente: <input type='text' name='codcl'  value='$obj->CodCliente' required></span>";
+echo "<span>DNI: <input type='text' name='dni' value='$obj->DNI' required></span>";
+echo "<span>Nombre: <input type='text' name='nom' value='$obj->Nombre' required></span>";
+echo "<span>Apellidos: <input type='text' name='ap' value='$obj->Apellidos' required></span>";
+echo "<span>Direccion: <input type='text' name='dir' value='$obj->Direccion' required></span>";
+echo "<span>Telefono: <input type='stext' name='tef' value='$obj->Telefono'required></span>";
+echo "<input type='submit' value='Actualizar'>";
+echo "</fieldset>";
+echo "</form>";
+
+    }
+
+} 
+else { 
+    echo "Error en consulta";
+}
+
+$result->close();
+unset($obj);
+unset($connection);
+
+?>
+<?php else: ?>
+
+    <?php
+
+//CREATING THE CONNECTION
+$connection = new mysqli("localhost", "tf", "123456", "tf");
+$connection->set_charset("utf8");
+
+//TESTING IF THE CONNECTION WAS RIGHT
+if ($connection->connect_errno) {
+    printf("Connection failed: %s\n", $connection->connect_error);
+    exit();
+}
+
+//MAKING A SELECT QUERY
+/* Consultas de selección que devuelven un conjunto de resultados */
+$query = "update clientes
+set CodCliente = '".$_POST['codcl']."',
+	DNI = '".$_POST['dni']."',
+    Nombre = '".$_POST['nom']."',
+    Apellidos = '".$_POST['ap']."',
+    Direccion = '".$_POST['dir']."',
+    Telefono = '".$_POST['tef']."' where CodCliente = '".$_POST['codcl']."'";
+if ($result = $connection->query($query)) {
+
+    header('Location:clientes.php');
+
+} else { 
+    echo "Error en consulta";
+}
+
+$result->close();
+unset($obj);
+unset($connection);
+
+?>
+
+<?php endif?>
+
+</body>
+</html>
+
