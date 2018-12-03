c77e829249f4e6838af67bc1566434a32a824a54
Alejandro314 <alejandro_roman_caballero@outlook.es>
2018-12-03 23:43:32 +0100

    Ejercicio_del_post_funcion_echo

--- PHP/Funciones/post_funcion/lista.php
+++ PHP/Funciones/post_funcion/lista.php
@@ -5,31 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
 </head>
 <body>
     <?php
-$_POST["cantidad"];
-
 function mostrar_lista($v) 
 {
 $lista = "<ul>"; 
-for ($i=0 ; $i < $_POST["cantidad"]; $i++ ) { 
-   $num=0;
-   $num= $num+$i;
-   if ($num%2==) {
-    $lista= $lista."<li>".$v."</li>";
 
-   }
-   
+
+for ($num=0 ; $num < ($_POST["cantidad"]*2); $num++ ) { 
+    
+    if ($num%2==0)
+    {
+
+    $lista= $lista."<li>".$num."</li>";
+
+    }
+
+
    
 }
 $lista=$lista."</ul>";
 return $lista;
 
 }
+
+if ($_POST == NULL) {
+    echo "<h1>No hay datos insertados en el POST</h1>";
+} else {
+
 echo mostrar_lista($_POST["cantidad"]);
+}
+
     ?>
 </body>
 </html>
